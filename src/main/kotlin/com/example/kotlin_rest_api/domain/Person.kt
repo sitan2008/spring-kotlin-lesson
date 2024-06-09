9c67b42b2827008a304cf8c2b0794f44c4ac3499
Oleksandr Furinets <furinets.o@ajax.systems>
2024-06-09 21:03:48 +0300

    Change PersonDao >>> PersonRepository

--- src/main/kotlin/com/example/kotlin_rest_api/domain/Person.kt
+++ src/main/kotlin/com/example/kotlin_rest_api/domain/Person.kt
@@ -1,5 +1,8 @@
 package com.example.kotlin_rest_api.domain
 
+import jakarta.persistence.Entity
+
+@Entity
 data class Person(
     val id: Long = 1,
     var name: String = "",
--- src/main/kotlin/com/example/kotlin_rest_api/service/PersonManagementServiceImpl.kt
+++ src/main/kotlin/com/example/kotlin_rest_api/service/PersonManagementServiceImpl.kt
@@ -2,7 +2,7 @@ package com.example.kotlin_rest_api.service
 
 import com.example.kotlin_rest_api.dto.AddPersonRequest
 import com.example.kotlin_rest_api.dto.PersonResponse
-import com.example.kotlin_rest_api.dao.PersonDao
+import com.example.kotlin_rest_api.dao.PersonRepository
 import com.example.kotlin_rest_api.domain.Person
 import com.example.kotlin_rest_api.dto.UpdatePersonRequest
 import com.example.kotlin_rest_api.transformer.AddPersonRequestTransformer
@@ -11,13 +11,13 @@ import org.springframework.stereotype.Service
 
 @Service
 class PersonManagementServiceImpl(
-    private val personDao: PersonDao,
+    private val personRepository: PersonRepository,
     private val addPersonRequestTransformer: AddPersonRequestTransformer
 ) : PersonManagementService {
 
     override fun findById(id: Long): PersonResponse? = this.findPersonById(id).toPersonResponse()
 
-    override fun findAll(): List<PersonResponse> = this.personDao.findAll().map(Person::toPersonResponse)
+    override fun findAll(): List<PersonResponse> = this.personRepository.findAll().map(Person::toPersonResponse)
 
     override fun save(addPersonRequest: AddPersonRequest): PersonResponse {
         return this.saveOrUpdate(
@@ -42,7 +42,7 @@ class PersonManagementServiceImpl(
         TODO("Not yet implemented")
     }
 
-    private fun findPersonById(id: Long): Person? = this.personDao.findByIdOrNull(id)
+    private fun findPersonById(id: Long): Person? = this.personRepository.findByIdOrNull(id)
 
-    private fun saveOrUpdate(person: Person): PersonResponse = this.personDao.save(person).toPersonResponse()
+    private fun saveOrUpdate(person: Person): PersonResponse = this.personRepository.save(person).toPersonResponse()
 }
