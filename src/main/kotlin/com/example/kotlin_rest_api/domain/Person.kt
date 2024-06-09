1cce31e36165a07806e34cf5b0b3d82e5d96a78f
Oleksandr Furinets <furinets.o@ajax.systems>
2024-06-09 20:58:34 +0300

    Part 4

--- src/main/kotlin/com/example/kotlin_rest_api/domain/Person.kt
+++ src/main/kotlin/com/example/kotlin_rest_api/domain/Person.kt
@@ -2,6 +2,6 @@ package com.example.kotlin_rest_api.domain
 
 data class Person(
     val id: Long = 1,
-    val name: String = "",
-    val lastName: String? = null,
+    var name: String = "",
+    var lastName: String? = null,
 )
--- src/main/kotlin/com/example/kotlin_rest_api/service/PersonManagementServiceImpl.kt
+++ src/main/kotlin/com/example/kotlin_rest_api/service/PersonManagementServiceImpl.kt
@@ -3,8 +3,10 @@ package com.example.kotlin_rest_api.service
 import com.example.kotlin_rest_api.dto.AddPersonRequest
 import com.example.kotlin_rest_api.dto.PersonResponse
 import com.example.kotlin_rest_api.dao.PersonDao
+import com.example.kotlin_rest_api.domain.Person
 import com.example.kotlin_rest_api.dto.UpdatePersonRequest
 import com.example.kotlin_rest_api.transformer.AddPersonRequestTransformer
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,23 +15,34 @@ class PersonManagementServiceImpl(
     private val addPersonRequestTransformer: AddPersonRequestTransformer
 ) : PersonManagementService {
 
-    override fun findById(id: Long): PersonResponse? {
-        TODO("Not yet implemented")
-    }
+    override fun findById(id: Long): PersonResponse? = this.findPersonById(id).toPersonResponse()
 
-    override fun findAll(): List<PersonResponse> {
-        TODO("Not yet implemented")
-    }
+    override fun findAll(): List<PersonResponse> = this.personDao.findAll().map(Person::toPersonResponse)
 
     override fun save(addPersonRequest: AddPersonRequest): PersonResponse {
-        TODO("Not yet implemented")
+        return this.saveOrUpdate(
+            addPersonRequestTransformer.transform(addPersonRequest)
+        )
     }
 
     override fun update(updatePersonRequest: UpdatePersonRequest): PersonResponse {
-        TODO("Not yet implemented")
+        val person = this.findPersonById(updatePersonRequest.id) ?: throw IllegalStateException(
+            "Can't find person with id: id=${updatePersonRequest.id}"
+        )
+
+        return this.saveOrUpdate(
+            person.apply {
+                this.name = updatePersonRequest.name
+                this.lastName = updatePersonRequest.lastName
+            }
+        )
     }
 
     override fun deleteById(id: Long) {
         TODO("Not yet implemented")
     }
+
+    private fun findPersonById(id: Long): Person? = this.personDao.findByIdOrNull(id)
+
+    private fun saveOrUpdate(person: Person): PersonResponse = this.personDao.save(person).toPersonResponse()
 }
