06d3434f110a5b71a3ddf79f43b5209a17b5e52f
Oleksandr Furinets <furinets.o@ajax.systems>
2024-06-17 23:35:37 +0300

    Rename PersonResource >>> PersonController

--- src/main/kotlin/com/example/kotlin_rest_api/repource/PersonController.kt
+++ src/main/kotlin/com/example/kotlin_rest_api/repource/PersonController.kt
@@ -3,7 +3,7 @@ package com.example.kotlin_rest_api.repource
 import com.example.kotlin_rest_api.dto.AddPersonRequest
 import com.example.kotlin_rest_api.dto.PersonResponse
 import com.example.kotlin_rest_api.dto.UpdatePersonRequest
-import com.example.kotlin_rest_api.repource.PersonResourceImpl.Companion.BASE_PERSON_URL
+import com.example.kotlin_rest_api.repource.PersonController.Companion.BASE_PERSON_URL
 import com.example.kotlin_rest_api.service.PersonManagementService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -17,34 +17,34 @@ import java.net.URI
 // Вказуємо шлях до ресурсу за який відповідає цей рест контроллер
 @RequestMapping(value = [BASE_PERSON_URL])
 // Імплементація PersonResource
-class PersonResourceImpl(
+class PersonController(
     private val personManagementService: PersonManagementService
-) : PersonResource {
+) {
 
     // <Get, Post, Path, Delete>_Mapping вказуємо які саме методи класу відповідають за певний http method
     @GetMapping("/{id}") // Ідентифікатор ресурсу
-    override fun findById(@PathVariable id: Long): ResponseEntity<PersonResponse?> {
+    fun findById(@PathVariable id: Long): ResponseEntity<PersonResponse?> {
         return ResponseEntity.status(HttpStatus.OK).body(this.personManagementService.findById(id))
     }
 
     @GetMapping("")
-    override fun findAll(pageable: Pageable): ResponseEntity<Page<PersonResponse>> {
+    fun findAll(pageable: Pageable): ResponseEntity<Page<PersonResponse>> {
         return ResponseEntity.status(HttpStatus.OK).body(this.personManagementService.findAll(pageable))
     }
 
     @PostMapping // @RequestBody вказує Spring що потрібно розпарсити body запиту, та прокинути його в аргумент методу
-    override fun save(@RequestBody addPersonRequest: AddPersonRequest): ResponseEntity<PersonResponse> {
+    fun save(@RequestBody addPersonRequest: AddPersonRequest): ResponseEntity<PersonResponse> {
         val personResponse = this.personManagementService.save(addPersonRequest)
         return ResponseEntity.created(URI.create(BASE_PERSON_URL.plus("/${personResponse.id}"))).body(personResponse)
     }
 
     @PutMapping
-    override fun update(@RequestBody updatePersonRequest: UpdatePersonRequest): ResponseEntity<PersonResponse> {
+    fun update(@RequestBody updatePersonRequest: UpdatePersonRequest): ResponseEntity<PersonResponse> {
         return ResponseEntity.status(HttpStatus.OK).body(this.personManagementService.update(updatePersonRequest))
     }
 
     @DeleteMapping("/{id}")
-    override fun deleteById(@PathVariable id: Long): ResponseEntity<Unit> {
+    fun deleteById(@PathVariable id: Long): ResponseEntity<Unit> {
         this.personManagementService.deleteById(id)
         return ResponseEntity.noContent().build()
     }
--- src/main/kotlin/com/example/kotlin_rest_api/service/PersonManagementService.kt
+++ src/main/kotlin/com/example/kotlin_rest_api/service/PersonManagementService.kt
@@ -2,15 +2,58 @@ package com.example.kotlin_rest_api.service
 
 import com.example.kotlin_rest_api.dto.AddPersonRequest
 import com.example.kotlin_rest_api.dto.PersonResponse
+import com.example.kotlin_rest_api.dao.PersonDao
+import com.example.kotlin_rest_api.domain.Person
 import com.example.kotlin_rest_api.dto.UpdatePersonRequest
+import com.example.kotlin_rest_api.transformer.AddPersonRequestTransformer
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
 
-// Service Layer Interface, тут проходить робота з бізнес-логікою, а також з Dao/Repository які відповідають за взаємодію з БД
-interface PersonManagementService {
-    fun findById(id: Long): PersonResponse?
-    fun findAll(pageable: Pageable): Page<PersonResponse>
-    fun save(addPersonRequest: AddPersonRequest): PersonResponse
-    fun update(updatePersonRequest: UpdatePersonRequest): PersonResponse
-    fun deleteById(id: Long): Unit
+
+//Імплементація PersonManagementService
+@Service
+class PersonManagementService(
+    private val personDao: PersonDao, private val addPersonRequestTransformer: AddPersonRequestTransformer
+) {
+
+    fun findById(id: Long): PersonResponse? {
+        val person = this.findPersonById(id) ?: throw IllegalStateException(
+            "Can't find person with id: id=${id}"
+        )
+
+        return person.toPersonResponse()
+    }
+
+    fun findAll(pageable: Pageable): Page<PersonResponse> =
+        this.personDao.findAll(pageable).map(Person::toPersonResponse)
+
+    fun save(addPersonRequest: AddPersonRequest): PersonResponse {
+        return this.saveOrUpdate(
+            addPersonRequestTransformer.transform(addPersonRequest)
+        )
+    }
+
+    fun update(updatePersonRequest: UpdatePersonRequest): PersonResponse {
+        val person = this.findPersonById(updatePersonRequest.id) ?: throw IllegalStateException(
+            "Can't find person with id: id=${updatePersonRequest.id}"
+        )
+
+        return this.saveOrUpdate(person.apply {
+            this.name = updatePersonRequest.name
+            this.lastName = updatePersonRequest.lastName
+        })
+    }
+
+    fun deleteById(id: Long): Unit {
+        val person = this.findPersonById(id) ?: throw IllegalStateException(
+            "Can't find person with id: id=${id}"
+        )
+        return this.personDao.deleteById(person.id)
+    }
+
+    private fun findPersonById(id: Long): Person? = this.personDao.findByIdOrNull(id)
+
+    private fun saveOrUpdate(person: Person): PersonResponse = this.personDao.save(person).toPersonResponse()
 }
